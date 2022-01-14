a3038224699f26e6fb824a2e9d045cd6db9a9c1c
Matthew O'Neill <matthew@matthewoneill.com>
2022-01-14 17:27:59 +0000

    some more stuff

---
+++ MySite2.WebApp/Controllers/RunsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MySite2.WebApp.Controllers
+{
+    public class RunsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MySite2.WebApp/Pages/Runs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MySite2.WebApp.Pages
+{
+    public class RunsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
